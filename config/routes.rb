bfa483dac45713505ed71ed0442533d2530dd265
EC2 Default User <ec2-user@ip-172-31-79-147.ec2.internal>
2021-10-10 13:28:23 +0000

    Add: top,about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :customers
   devise_for :admins
   devise_for :costomers
 
   root to: 'homes#top'
+  get "/home/about" => "homes#about"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_10_104257) do
+ActiveRecord::Schema.define(version: 2021_10_10_123233) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_10_10_104257) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "costomers", force: :cascade do |t|
+  create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 2021_10_10_104257) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_costomers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_costomers_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
 end
