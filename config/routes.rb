c8d21f3ca9682b640edc96c6fb4b59fc98dec9a4
EC2 Default User <ec2-user@ip-172-31-79-147.ec2.internal>
2021-10-13 13:54:41 +0000

    Edit: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,12 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'orders_details/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    resources :customers
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/creat'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
+    resources :orders_details, only: [:update]
+    resources :orders, only: [:show, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
     get 'homes/top'
   end
-
 
 
 
