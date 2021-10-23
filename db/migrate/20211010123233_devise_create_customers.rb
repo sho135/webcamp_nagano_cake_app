d2024f03e79c574c8c9a2bc11e343c8da51da44e
sho135 <sh135pro@gmail.com>
2021-10-23 09:23:29 +0000

    Edit: migrate

--- db/migrate/20211010123233_devise_create_customers.rb
+++ db/migrate/20211010123233_devise_create_customers.rb
@@ -33,12 +33,12 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
       t.string :last_name
-    　t.string :first_name
-    　t.string :last_name_kana
-    　t.string :first_name_kana
-    　t.string :postal_code
-    　t.string :address
-    　t.string :telephone_number
+      t.string :first_name
+      t.string :last_name_kana
+      t.string :first_name_kana
+      t.string :postal_code
+      t.string :address
+      t.string :telephone_number
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -47,8 +47,6 @@ ActiveRecord::Schema.define(version: 2021_10_13_120258) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "last_name"
     t.string "first_name"
     t.string "last_name_kana"
@@ -56,6 +54,8 @@ ActiveRecord::Schema.define(version: 2021_10_13_120258) do
     t.string "postal_code"
     t.string "address"
     t.string "telephone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "is_active"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
