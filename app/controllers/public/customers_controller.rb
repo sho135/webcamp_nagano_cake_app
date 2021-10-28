905260166e22f7c47d782369ea63f8b4add55ea1
sho135 <sh135pro@gmail.com>
2021-10-28 03:09:05 +0000

    Edit: views_public_thankspage

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -19,20 +19,25 @@ class Public::CustomersController < ApplicationController
   end
   end
 
-  def unsubscribe  
+  # 顧客の退会確認画面
+  def unsubscribe
     @customer = current_customer
   end
 
+  # 顧客の退会処理(ステータスの更新)
   def withdraw
     @customer = current_customer
-    
+    @customer.update(is_active: true)
+    reset_session
+    flash[:notice] = "ご利用ありがとうございました。"
+    redirect_to root_path
   end
 
 
   private
 
   def customer_params
-    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_active)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_active)
   end
 
 end
