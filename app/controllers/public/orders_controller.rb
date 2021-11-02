26633dfcea060d1cda3b519055fae69fd23c9a5a
sho135 <sh135pro@gmail.com>
2021-11-02 00:42:27 +0000

    Edit: controller_piblic_orders

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,11 +10,42 @@ class Public::OrdersController < ApplicationController
     @order.customer_id = current_customer.id
     @cart_items = current_customer.cart_items
     @sum = 0
+
+  if params[:order][:select_option] == "0"
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = full_name(current_customer)
+  elsif params[:order][:select_option] == "1"
+    @order.postal_code = Addrss.find(order_params[:customer]).postal_code
+    @order.address = Addrss.find(order_params[:customer]).address
+    @order.name = Addrss.find(order_params[:customer]).name
+  elsif params[:order][:select_option] == "2"
+    @order.customer_address = order_params[:customer_address]
+    @order.customer_postal_code = order_params[:customer_postal_code]
+    @order.customer_name = order_params[:customer_name]
+  end
   end
 
   def create
-    @order = current_customer.orders.new(order_params)
+    @order = Order.new(order_params)
+    @order.customer = current_customer
+    @order.customer_id = current_customer.id
     @order.save
+
+    @cart_items = current_customer.cart_items
+    @cart_items.each do |cart_item|
+      @order_detail = OrderDetail.new
+      @order_detail.item_id = cart_item.item_id
+      @order_detail.order_id = @order.id
+      @order_detail.amount = cart_item.amount
+      @order_detail.price = cart_item.item.price * cart_item.amount
+      @order_detail.save
+    end
+
+    @cart_items.destroy_all
+
+    redirect_to thanks_public_orders_path
+
   end
 
   def thanks
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,7 +3,7 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
-  varidates :item_id, :order_id, :amount, :price, presence: true
+  validates :item_id, :order_id, :amount, :price, presence: true
 
   enum making_status: {"着手不可": 0,"制作待ち": 1,"製作中": 2,"制作完了": 3}
 
