52ed679ea426a506e8e28e55f1ffd4d51a86906c
sho135 <sh135pro@gmail.com>
2021-11-05 10:39:11 +0000

    Edit views public_top

--- app/controllers/concerns/homes_controller.rb
+++ app/controllers/concerns/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
 
 def top
+  @new_items = Item.last(4)
 end
 
 def about
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,7 +6,15 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItems.new
   end
 
+
+
+  private
+
+  def items_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image_id)
+  end
 
 end
