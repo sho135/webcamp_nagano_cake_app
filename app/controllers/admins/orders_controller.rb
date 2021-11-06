21860375e379a51dd6bffbca0789fe16bc04130c
sho135 <sh135pro@gmail.com>
2021-11-06 15:23:57 +0000

    Edit: views admins_orders_show

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -4,6 +4,7 @@ class Admins::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
+    @sum = 0
   end
 
   def update
