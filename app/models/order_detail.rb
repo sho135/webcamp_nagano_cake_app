10f9d261a573c8fba9dbabc5ae29e5684a09ad4c
sho135 <sh135pro@gmail.com>
2021-11-10 13:39:10 +0000

    Edit: models views :order orderdetail

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -5,6 +5,10 @@ class OrderDetail < ApplicationRecord
 
   validates :item_id, :order_id, :amount, :price, presence: true
 
+  def subtotal
+    item.with_tax_price * amount
+  end
+
   enum making_status: {
     着手不可: 0,
     制作待ち: 1,
